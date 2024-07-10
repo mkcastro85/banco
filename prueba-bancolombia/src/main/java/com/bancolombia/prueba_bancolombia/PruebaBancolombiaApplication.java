40e5c379598f104692cc5dd490ec7e35db882f3f
Mey_castro <meyling.castro@zabud.com.co>
2024-07-10 13:02:20 -0500

    prime comit

---
+++ prueba-bancolombia/src/main/java/com/bancolombia/prueba_bancolombia/PruebaBancolombiaApplication.java
@@ -0,0 +1,13 @@
+package com.bancolombia.prueba_bancolombia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBancolombiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBancolombiaApplication.class, args);
+	}
+
+}
---
+++ prueba-bancolombia/src/main/java/com/bancolombia/prueba_bancolombia/repositories/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.bancolombia.prueba_bancolombia.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bancolombia.prueba_bancolombia.models.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+}
